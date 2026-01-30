4124637f827b553e03b44cad00e02beea7e7b6d8
“Peteroche” <petergoddey08l@gmail.com>
2026-01-30 22:11:37 +0100

    resolve conflict

--- nftopia-backend/src/main.ts
+++ nftopia-backend/src/main.ts
@@ -46,7 +46,9 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}/api/v1`);
-  console.log(`Swagger documentation available at: http://localhost:${port}/api/docs`);
+  console.log(
+    `Swagger documentation available at: http://localhost:${port}/api/docs`,
+  );
 }
 void bootstrap().catch((err) => {
   console.error('Error during bootstrap', err);
